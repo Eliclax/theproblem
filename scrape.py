796dc851d6f31c65d3796cd3090aaabc8b63f556
Xutong Wang <TW2000x@gmail.com>
2021-09-08 19:53:11 +1200

    now supports italics, bold, itemize, and enumerate. html_to_latex no longer takes tag argument.

--- scrape.py
+++ scrape.py
@@ -5,25 +5,88 @@ import pandas as pd
 problems=[]
 proposers=[]
 statement=""
+c=0
 
 #driver = webdriver.Chrome("/snap/chromium/current/usr/lib/chromium-browser/chromedriver")
 #driver.get("https://artofproblemsolving.com/community/c1306546_2020imo")
 #content = driver.page_source
 #soup = BeautifulSoup(content)
 
+
 with open("imo2020.html") as imo2020:
-    soup = BeautifulSoup(imo2020, 'html.parser')
+	soup = BeautifulSoup(imo2020, 'html.parser')
+
+#imo2020 = "<i><b>DAY 1</b></i>"
+#soup = BeautifulSoup(imo2020, 'html.parser')
+
+def html_to_latex(temp_html = ""):
+	#print("===========================")
+	if temp_html == "":
+		print("GOT EMPTY STRING!!")
+		return ""
+
+	temp_soup = BeautifulSoup(temp_html, 'html.parser')
+	tag = ""
+	temp_statement = ""
+
+	# print("temp_soup: ")
+	# print(str(temp_soup))
+	# print("temp_soup.find(): ")
+	# print(str(temp_soup.find()))
+	# print("type(temp_soup.find()): ")
+	# print(type(temp_soup.find()))
+	# print("temp_soup.find(string=True): ")
+	# print(str(temp_soup.find(string=True)))
+	# print("type(temp_soup.find(string=True)): ")
+	# print(type(temp_soup.find(string=True)))
+
+	
+	if isinstance(temp_soup.find(), Tag):
+		tag = temp_soup.find().name
+		if temp_soup.find().name == 'img':
+			temp_statement += temp_soup.find().get('alt')
+		for b in temp_soup.find().children:
+			#print("b: ", str(b))
+			temp_statement += html_to_latex(str(b))
+	else:
+		temp_statement += temp_soup.find(string=True)
+		
+	if tag == "i":
+		temp_statement = '\\textit{' + temp_statement + '}'
+	elif tag == "b":
+		temp_statement = '\\textbf{' + temp_statement + '}'
+	elif tag == "ul":
+		temp_statement = '\\begin{itemize}' + temp_statement + '\\end{itemize}'
+	elif tag == "ol":
+		temp_statement = '\\begin{enumerate}' + temp_statement + '\\end{enumerate}'
+	elif tag == "li":
+		temp_statement = '\\item ' + temp_statement
+
+	#print(temp_statement)
+	return temp_statement
+
+#problems.append(html_to_latex(str(soup)))
+#print(problems)
 
 for a in soup.find_all('div', class_="cmty-view-post-item-text"):
-	prob = BeautifulSoup(str(a), 'html.parser')
-	statement=""
-	#print(list(prob.div.next_elements))
-	for desc in prob.div.next_elements:
-		if isinstance(desc, NavigableString):
-			statement += str(desc)
-		elif desc.get('alt') is not None:
-			statement += str(desc.get('alt'))
+	statement = html_to_latex(str(a))
 	print(statement)
+	problems.append(statement)
 
-#df = pd.DataFrame({'Statement':problems,'Proposer':proposers}) 
-#df.to_csv('products.csv', index=False, encoding='utf-8')
+#print(problems[3])
+
+
+
+
+	# prob = BeautifulSoup(str(a), 'html.parser')
+	# statement = ""
+	# for desc in prob.div.next_elements:
+	# 	print(desc)
+	# 	if isinstance(desc, NavigableString):
+	# 		statement += str(desc)
+	# 	elif desc.get('alt') is not None:
+	# 		statement += str(desc.get('alt'))
+	# print(statement)
+
+df = pd.DataFrame({'Statement':problems}) 
+df.to_csv('imo2020.csv', index=False, encoding='utf-8')
